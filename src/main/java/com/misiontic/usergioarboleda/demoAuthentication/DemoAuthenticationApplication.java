25762c7d5aac3f24eb343b5a2e47323210db3055
casierrav-sa <carlos.sierra1.mt@usa.edu.co>
2022-10-01 11:47:48 -0500

    Main class to execute application

---
+++ src/main/java/com/misiontic/usergioarboleda/demoAuthentication/DemoAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.usergioarboleda.demoAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAuthenticationApplication.class, args);
+	}
+
+}
